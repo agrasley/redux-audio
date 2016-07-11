d9ac34c2a4bd40eae018f58eb9a20ac8f76f0ee9
Alex Grasley <alex@grasley.com>
2016-07-11 11:21:21 -0700

    remove id attr

--- src/Audio.js
+++ src/Audio.js
@@ -47,7 +47,7 @@ const Audio = React.createClass({
   },
 
   render () {
-    const { autoPlay, controls, loop, preload, src, id } = this.props
+    const { autoPlay, controls, loop, preload, src } = this.props
 
     return (
       <audio
@@ -56,7 +56,6 @@ const Audio = React.createClass({
         loop={loop}
         preload={preload}
         src={src}
-        id={id}
       />
     )
   },
